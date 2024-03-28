c2f3ad2b30915847f1d13959367a6e050719484c
matheus-AR <matheusaraujorodrigues@outlook.com>
2024-03-28 15:04:42 -0300

    Adicionando navegação dos componente para a page content

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-small-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './small-card.component.html',
   styleUrl: './small-card.component.css'
 })
@@ -14,6 +15,10 @@ export class SmallCardComponent implements OnInit {
 
   @Input()
   cardTitle: string = ""
+
+  @Input()
+  Id: string = "0"
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-content',
@@ -12,8 +12,14 @@ export class ContentComponent implements OnInit {
   photoCover: string = "https://imagenes.20minutos.es/files/image_1920_1080/uploads/imagenes/2019/12/15/iron-man.jpg"
   contentTitle: string = "NOTICIA EXEMPLO"
   contentDescription: string = "bla bla bla bla"
+
+  constructor(
+    private route: ActivatedRoute
+  ){ }
   ngOnInit(): void {
-    console.log('Method not implemented.');
+    this.route.paramMap.subscribe( value =>
+      console.log(value.get("id"))
+    )
   }
 
 }
