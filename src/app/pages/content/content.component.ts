06ce74b8603c022bac5cbe79438ec86641a695eb
matheus-AR <matheusaraujorodrigues@outlook.com>
2024-03-28 15:36:38 -0300

    Tornando a content page dinâmica

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
+import { dataFake } from '../../../data/dataFake';
 
 @Component({
   selector: 'app-content',
@@ -12,14 +13,25 @@ export class ContentComponent implements OnInit {
   photoCover: string = "https://imagenes.20minutos.es/files/image_1920_1080/uploads/imagenes/2019/12/15/iron-man.jpg"
   contentTitle: string = "NOTICIA EXEMPLO"
   contentDescription: string = "bla bla bla bla"
+  private id: string | null = "0"
 
   constructor(
     private route: ActivatedRoute
   ){ }
   ngOnInit(): void {
     this.route.paramMap.subscribe( value =>
-      console.log(value.get("id"))
+      this.id = value.get("id")
     )
+
+    this.setValuesToComponent(this.id);
+  }
+
+  setValuesToComponent(id: string | null) {
+    const result = dataFake.filter(article => article.id == id)[0];
+
+    this.contentTitle = result.title;
+    this.contentDescription = result.description;
+    this.photoCover = result.photoCover;
   }
 
 }
--- src/data/dataFake.ts
+++ src/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
   {
-    "id": 1,
-    "photo": "aaa",
+    "id": "1",
     "title": "saiu novo filme",
-    "description": "ola"
+    "description": "Marvel studios anuncia novo fimel do homem de ferro.",
+    "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/80C64C0B63382CD3ED2653356125F275F63D036028A77D38DC3286AD851A6833/scale?width=1200&aspectRatio=1.78&format=webp"
   },
   {
-    "id": 2,
-    "photo": "aaa",
-    "title": "saiu novo filme",
-    "description": "ola"
+    "id": "2",
+    "title": "Nova séria anunciada",
+    "description": "Marvel studios anuncia novo fimel dos vingadores.",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3q-Iry70s0bKQJoy69QFeS56YPBA0rnPgyw&usqp=CAU"
+  },
+  {
+    "id": "3",
+    "title": "Novo filme anunciado do pantera negra",
+    "description": "Marvel studios anuncia novo fimel do pantera negra.",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWkYwLUaL--JF2dMaiCApxnWhjWgAbO33oqQw_LifDKZyEw62JVcyChdLPyzP0TBJEBuw&usqp=CAU"
+  },
+  {
+    "id": "4",
+    "title": "Novo filme anunciado do X-Men",
+    "description": "Marvel studios anuncia novo fimel do X-men.",
+    "photoCover": "https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/820x547/1_xmen-26096415.jpeg?20220722171339?20220722171339"
   }
 ];
